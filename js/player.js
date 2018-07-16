bfbd2ae13762cf4673eff085c90e490c7bb4869e
yuu <weiyuu95@gmail.com>
2018-07-16 21:46:03 +0800

    '播放js'

---
+++ js/player.js
@@ -0,0 +1,17 @@
+(function (window) {
+    function Player() {
+        return new Player.prototype.init();
+
+
+    }
+    Player.prototype = {
+        constructor:Player,
+        init:function () {
+
+        }
+    };
+
+    Player.prototype.init.prototype = Player.prototype
+    window.Player = Player;
+
+})(window);
