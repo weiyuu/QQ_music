101c6d11a4c0066891773dae9f4896489064271c
yuu <weiyuu95@gmail.com>
2018-07-16 23:07:03 +0800

    '播放交互'

--- js/player.js
+++ js/player.js
@@ -1,17 +1,37 @@
 (function (window) {
-    function Player() {
-        return new Player.prototype.init();
+    function Player($audio) {
+        return new Player.prototype.init($audio);
 
 
     }
     Player.prototype = {
         constructor:Player,
-        init:function () {
-
+        musicList:[],
+        currentIndex: -1,
+        init:function ($audio) {
+            this.$audio = $audio;
+            this.audio = $audio.get(0);
+        },
+        playMusic:function (index, music) {
+            // 判断是否为同一首音乐
+            if (this.currentIndex == index) {
+                // 同一首歌曲
+                if(this.audio.paused) {
+                    this.audio.play();
+                }else {
+                    this.audio.pause();
+                }
+            }else {
+                // 非同一首歌
+                this.$audio.attr('src',music.link_url);
+                this.audio.play();
+            }
         }
     };
 
-    Player.prototype.init.prototype = Player.prototype
+    Player.prototype.init.prototype = Player.prototype;
     window.Player = Player;
 
 })(window);
+
+
